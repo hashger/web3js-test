e8b76b72c70b0af0f92b94720eda8117ec70eed5
mengeroshi <dpllgr@hotmail.com>
2020-06-22 22:42:37 -0500

    Create Account and Get Balance added

---
+++ createAcount.js
@@ -0,0 +1,5 @@
+const Web3 = require('web3');
+
+let url = 'https://mainnet.infura.io/v3/3fece7f41eef4ec3a6df8be272f03848';
+let web3 = new Web3(url);
+console.log(web3.eth.accounts.create());
---
+++ getBalance.js
@@ -0,0 +1,15 @@
+let Web3 = require('web3');
+
+let url = 'https://mainnet.infura.io/v3/3fece7f41eef4ec3a6df8be272f03848';
+let web3 = new Web3(url);
+
+let address = '0x742d35Cc6634C0532925a3b844Bc454e4438f44e';
+/*
+console.log(web3.eth.getBalance(address,(err, bal)=> {balance = bal})); */
+web3.eth.getBalance(address,function(err, bal){
+    {balance = bal};
+    console.log('Wei',balance);
+    etherBalance = web3.utils.fromWei(balance, 'ether');
+    console.log('Ethers: ',etherBalance);
+});
+
